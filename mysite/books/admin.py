74a8c698da227a42a4fb6133b4b784690b53a99e
linwenjunid <linwenjunid@163.com>
2019-01-09 17:13:37 +0800

    1、增加last_update_time 2、增加模型Tag 3、视图增加相应的处理及优化

--- mysite/books/admin.py
+++ mysite/books/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from .models import Publisher, Author, Book
+from django.http import HttpResponseRedirect
+from .models import Publisher, Author, Book, Tag
 from django.contrib.auth.models import User
+
+
+class TagInline(admin.TabularInline):
+    model = Tag
+    extra = 1
 
 
 @admin.register(Publisher)
 class PublisherAdmin(admin.ModelAdmin):
     list_display = ('name','address','city','state_province','country','website')
+    list_editable = ('address',)
 
 
 @admin.register(Author)
@@ -16,6 +23,54 @@ class AuthorAdmin(admin.ModelAdmin):
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    # 自定义动作 复制一条记录
+    def copy_one(self, request, queryset):
+        if queryset.count() == 1:
+            old_data = queryset.values()[0]
+            old_data.pop('id')
+            new_data = Book.objects.create(**old_data)
+            return HttpResponseRedirect('{}{}/change'.format(request.path, new_data.id))
+        else:
+            self.message_user(request, "只能选取一条数据！")
+    copy_one.short_description = '复制所选数据'
+    actions = [copy_one]
+
+    # 不同的用户显示不同的操作
+    def get_actions(self, request):
+        actions = super(BookAdmin, self).get_actions(request)
+        if request.user.is_superuser:
+            if 'copy_one' in actions:
+                del actions['copy_one']
+        return actions
+
+    # 不同的用户显示不同的内容
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        if request.user.is_superuser:
+            self.fieldsets = [
+                ('基础信息',{'fields': ['title','authors','master']}),
+                ('出版信息',{'fields': ['publisher', 'publication_date']}),
+            ]
+        else:
+            self.fieldsets = [
+                ('基础信息', {'fields': ['title', 'authors']}),
+                ('出版信息', {'fields': ['publisher', 'publication_date']}),
+            ]
+        return super(BookAdmin, self).change_view(request, object_id, form_url, extra_context=extra_context)
+
+    # 保存时自动
+    def save_model(self, request, obj, form, change):
+        if not obj.master:
+            obj.master = request.user
+        super(BookAdmin, self).save_model(request, obj, form, change)
+
+    readonly_fields = ['master',]
+
+    # 重新定义此函数，限制普通用户所能修改的字段
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            self.readonly_fields = []
+        return self.readonly_fields
+
     # 通过当前登录的用户过滤显示的数据
     def get_queryset(self, request):
         qs = super(BookAdmin, self).get_queryset(request)
@@ -28,6 +83,7 @@ class BookAdmin(admin.ModelAdmin):
         return ", ".join([x.__str__() for x in self.authors.all()])
 
     get_authors.short_description = '作者'
+
     # 颜色显示
     def get_title(self):
         from django.utils.html import format_html
@@ -40,13 +96,13 @@ class BookAdmin(admin.ModelAdmin):
                 '<span style="color: green;">{}</span>',
                 '有效')
 
-    get_title.short_description = '书名'
+    get_title.short_description = '状态'
 
-    list_display = ('id','title' ,get_authors, 'publisher', 'publication_date', get_title)
+    list_display = ('id','title' ,get_authors, 'publisher', 'publication_date', get_title, 'last_update_time')
     # 设置哪些字段可以点击进入编辑界面
     list_display_links = ('id', 'title')
     # list_per_page设置每页显示多少条记录，默认是100条
-    list_per_page = 50
+    list_per_page = 5
     # 筛选器
     list_filter = ('publication_date', 'authors', 'publisher')
     # 搜索字段
@@ -55,12 +111,22 @@ class BookAdmin(admin.ModelAdmin):
     date_hierarchy = 'publication_date'
     # ordering设置默认排序字段，负号表示降序排序
     ordering = ('-publication_date',)
-    fields = ('title', 'authors', 'publisher', 'publication_date','master')
-    filter_horizontal = ('authors',)#filter_vertical垂直排布 多项项过多时的选择
-    #raw_id_fields = ('publisher',) #单选项过多时的选择
+    # filter_vertical垂直排布 多项项过多时的选择
+    filter_horizontal = ('authors',)
+    # 单选项过多时的选择
+    # raw_id_fields = ('publisher',)
+
+    # 用fields或exclude控制显示或者排除的字段
+    # fields = ('title', 'authors', ('publisher', 'publication_date'),'master')
+    fieldsets = [
+        ('基础信息',{'fields': ['title','authors','master']}),
+        ('出版信息',{'fields': ['publisher', 'publication_date']}),
+    ]
+
+    # 主从表处理 扩展处理
+    inlines = [TagInline]
+
 # Register your models here.
-
-
 # admin.site.register(Publisher, PublisherAdmin)
 # admin.site.register(Author, AuthorAdmin)
 # admin.site.register(Book, BookAdmin)
--- mysite/books/models.py
+++ mysite/books/models.py
@@ -24,9 +24,14 @@ class Author(models.Model):
     first_name = models.CharField('名', max_length=30)
     last_name = models.CharField('姓', max_length=40)
     email = models.EmailField('邮箱', blank=True)
+    nation = models.CharField('国籍', max_length=20, default='中国')
 
     def __str__(self):
-        return u'%s %s' % (self.first_name, self.last_name)
+        if self.nation == '中国':
+            str = u'%s %s' % (self.last_name, self.first_name)
+        else:
+            str = u'%s %s' % (self.first_name, self.last_name)
+        return str
 
     class Meta:
         ordering = ['first_name']#默认排序字段
@@ -37,11 +42,12 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField('书名', max_length=100)
     authors = models.ManyToManyField(Author, verbose_name = '作者')
-    publisher = models.ForeignKey(Publisher, verbose_name = '出版商', on_delete=models.PROTECT)
+    publisher = models.ForeignKey(Publisher, verbose_name = '出版商', on_delete = models.PROTECT)
     # 设置时间字段为空必须同时指定 blank=True, null=True
     publication_date = models.DateField('出版日期', blank=True, null=True)
     num_pages = models.IntegerField(blank=True, null=True)
-    master = models.ForeignKey(User, verbose_name = '责任人', on_delete=models.PROTECT, null=True)
+    master = models.ForeignKey(User, verbose_name = '责任人', on_delete = models.PROTECT, null = True)
+    last_update_time = models.DateTimeField('最后一次更新日期', auto_now=True)
 
     def __str__(self):
         return self.title
@@ -50,3 +56,16 @@ class Book(models.Model):
         ordering = ['title']#默认排序字段
         verbose_name = '书籍'
         verbose_name_plural = '书籍'
+
+
+class Tag(models.Model):
+    tag = models.CharField('关键字', max_length=20)
+    book = models.ForeignKey(Book, verbose_name='关键字', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.tag
+
+    class Meta:
+        ordering = ['tag']
+        verbose_name = '关键字'
+        verbose_name_plural = '关键字'
