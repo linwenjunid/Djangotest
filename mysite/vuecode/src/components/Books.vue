65c523bd15802713f2ebd115d3a1b51cccc020f1
linwenjunid <linwenjunid@163.com>
2019-02-12 16:15:52 +0800

    vue

---
+++ mysite/vuecode/src/components/Books.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="Books">
+    <h1>书单{{count}}</h1>
+    <ol>
+    <li v-for="book in books" >
+      {{ book}}
+    </li>
+    <li>{{next}}</li>
+    <li>{{previous}}</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Books',
+  data () {
+    return {
+      books: null,
+      count:0,
+      next:0,
+      previous:0,
+    }
+  },
+  mounted () {
+    this.axios
+      .get('http://127.0.0.1:8000/books/api/books/')
+      .then(response => (
+        this.books = response.data.results,
+        this.count = response.data.count,
+        this.next = response.data.next,
+        this.previous = response.data.previous
+      ))
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ mysite/vuecode/src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+import Books from '@/components/Books'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Books',
+      component: Books
+    }
+  ]
+})
