656b7dc949bfab3a3b4c59f760d512bac0ea8de5
linwenjunid <linwenjunid@163.com>
2019-02-13 09:29:51 +0800

    翻页

--- mysite/vuecode/src/components/Books.vue
+++ mysite/vuecode/src/components/Books.vue
@@ -5,8 +5,10 @@
     <li v-for="book in books" >
       {{ book}}
     </li>
-    <li>{{next}}</li>
-    <li>{{previous}}</li>
+    <li>
+    <button v-on:click="go(previous)">上一页</button>
+    <button v-on:click="go(next)">下一页</button>
+    </li>
     </ol>
   </div>
 </template>
@@ -20,6 +22,18 @@ export default {
       count:0,
       next:0,
       previous:0,
+    }
+  },
+  methods: {
+    go:function(url) {
+      this.axios
+      .get(url)
+      .then(response => (
+        this.books = response.data.results,
+        this.count = response.data.count,
+        this.next = response.data.next,
+        this.previous = response.data.previous
+      ))
     }
   },
   mounted () {
